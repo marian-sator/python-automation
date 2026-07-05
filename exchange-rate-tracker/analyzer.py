68a4ab476260f09bdb2095e2d2a2554a56af2dc4
marian-sator <marian.sator@gmail.com>
2026-07-05 20:57:54 +0200

    Add analyzer for exchange rate statistics

---
+++ exchange-rate-tracker/analyzer.py
@@ -0,0 +1,18 @@
+import sqlite3
+import os 
+
+db = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + "\\database.db")
+
+cursor = db.cursor()
+
+cursor.execute("SELECT timestamp, MIN(rate) FROM exchange_rates")
+timestamp, min_rate = cursor.fetchone()
+print(f"{timestamp} Lowest rate: 1$ = {min_rate}€")
+
+cursor.execute("SELECT timestamp, MAX(rate) FROM exchange_rates")
+timestamp, max_rate = cursor.fetchone()
+print(f"{timestamp} Highest rate: 1$ = {max_rate}€")
+
+cursor.execute("SELECT AVG(rate) FROM exchange_rates")
+avg_rate = cursor.fetchone()[0]
+print(f"Average: 1$ = {avg_rate}€")
